dc7345692964c9957af1be72be30a1e862b75d19
dongchao612 <3224318157@qq.com>
2023-05-13 17:14:55 +0800

    >

---
+++ Part01/ch01/hello_client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handing(char *meeeage);
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct sockaddr sockaddr;
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    sockaddr_in serv_addr;
+    char message[30];
+    int str_len;
+
+    if (argc != 3)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0); // 创 建套接字,但此时套接字并不马上分为服务器端和客户端。
+    if (sock == -1)
+    {
+        error_handing("socket() error");
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family - AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = (atoi(argv[2]));
+
+    if (connect(sock, (sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) // 调用 connect函数向服务器端发送连接请求。
+    {
+        error_handing("bind() error");
+    }
+
+    str_len = read(sock, message, sizeof(message) - 1);
+
+    if (str_len == -1)
+    {
+        error_handing("read() error");
+    }
+
+    printf("Message from server : %s \n", message);
+    close(sock);
+
+    return 0;
+}
+void error_handing(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # TCPIPCode
-TCPIPCode
+《TCP&IP网络编程》图书信息：
+作者：尹圣雨
+
+
+## Part01 开始网络编程
+### ch01 理解网络编程和套接字
+```c
+//  hello_server.c
+int socket (int __domain, int __type, int __protocol);
+int bind (int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len);
+int listen (int __fd, int __n) ;
+int accept (int __fd, __SOCKADDR_ARG __addr,socklen_t *__restrict __addr_len);
+ssize_t write (int __fd, const void *__buf, size_t __n);
+```
+
+```c
+//  hello_clent.c
+int socket (int __domain, int __type, int __protocol);
+int connect (int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len);
+ssize_t read (int __fd, void *__buf, size_t __nbytes)
+```
