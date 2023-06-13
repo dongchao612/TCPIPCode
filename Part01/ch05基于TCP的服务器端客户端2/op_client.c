8e7eaced097b128abf9c0490eac2562fc5dc7609
dongchao612 <3224318157@qq.com>
2023-06-13 19:43:26 +0800

    >>

--- Part01/ch05基于TCP的服务器端客户端2/op_client.c
+++ Part01/ch05基于TCP的服务器端客户端2/op_client.c
@@ -1,76 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <string.h>
+
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define BUF_SIZE 1024
-#define RLT_SIZE 4
-#define OPSZ 4
+#define RLT_SIZE 4 // 待计算数字的字节数
+#define OPSZ 4     // 运算结果的字节数
 
-void error_handing(char *meeeage);
+void error_handling(char *message);
 
-typedef struct sockaddr_in sockaddr_in;
-typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in  sockaddr_in;
 
 int main(int argc, char *argv[])
 {
     int sock;
-    char opmsg[BUF_SIZE];
+    char opmsg[BUF_SIZE]; // 为收发数据准备的内存空间，需要数据积累到一定程度后再收发，因此通过数组创建
     int result, opnd_cnt, i;
-
-    sockaddr_in serv_adr;
+    struct sockaddr_in serv_adr;
 
     if (argc != 3)
     {
-        printf("Usage: %s <port>\n", argv[0]);
+        printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
-    sock = socket(PF_INET, SOCK_STREAM, 0); // 创 建套接字,但此时套接字并不马上分为服务器端和客户端。
+    sock = socket(PF_INET, SOCK_STREAM, 0);
     if (sock == -1)
     {
-        error_handing("socket() error");
+        error_handling("socket() error");
     }
 
     memset(&serv_adr, 0, sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_adr.sin_port = (atoi(argv[2]));
+    serv_adr.sin_port = htons(atoi(argv[2]));
 
-    if (connect(sock, (sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) // 调用 connect函数向服务器端发送连接请求。
+    if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
     {
-        error_handing("connect() error");
+        error_handling("connect() error!");
     }
     else
     {
-        puts("Connected..........");
+        puts("Connected.......");
     }
 
-    fputs("Operand count : ", stdout);
+    fputs("Operand count: ", stdout);
     scanf("%d", &opnd_cnt);
-    opmsg[0] = (char)opnd_cnt;
+    opmsg[0] = (char)opnd_cnt; // 从用户输入中得到待算数个数后，保存至数组opmsg
 
     for (i = 0; i < opnd_cnt; i++)
     {
-        printf("Operand %d:", i + 1);
-        scanf("%d", (int *)&opmsg[i * OPSZ + 1]);
+        printf("Operand %d: ", i + 1);
+        scanf("%d", (int *)&opmsg[i * OPSZ + 1]); // 待算数是4个字节，需要转化为int指针类型
     }
-    fgetc(stdin);
+    fgetc(stdin); // 删掉缓冲中的字符\n
     fputs("Operator: ", stdout);
-    scanf("%c", &opmsg[opnd_cnt * OPSZ + 1]);
-    write(sock, opmsg, opnd_cnt * OPSZ + 2);
-    read(sock,&result,RLT_SIZE);
+    scanf("%c", &opmsg[opnd_cnt * OPSZ + 1]); // 输入运算符信息，保存到opmsg数组
+    write(sock, opmsg, opnd_cnt * OPSZ + 2);  // 一次性传输opmsg数组中的相关信息
+    read(sock, &result, RLT_SIZE);            // 保存服务器端的运算结果。待接收的数据长度为4字节，调用一次read函数即可
 
-    printf("Operation result: %d",result);
+    printf("Operation result: %d \n", result);
     close(sock);
-
     return 0;
 }
 
-void error_handing(char *message)
+void error_handling(char *message)
 {
     fputs(message, stderr);
     fputc('\n', stderr);
--- Part01/ch05基于TCP的服务器端客户端2/op_server.c
+++ Part01/ch05基于TCP的服务器端客户端2/op_server.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <string.h>
+
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define BUF_SIZE 1024
-#define RLT_SIZE 4
-#define OPSZ 4
+#define OPSZ 4 // 运算结果的字节数
 
-int calculate(int opnum, int opnds[], char oprator);
-
-void error_handing(char *meeeage);
+void error_handling(char *message);
+int calculate(int opnum, int opnds[], char operator);
 
 typedef struct sockaddr_in sockaddr_in;
-typedef struct sockaddr sockaddr;
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
+
     char opinfo[BUF_SIZE];
     int result, opnd_cnt, i;
     int recv_cnt, recv_len;
 
     sockaddr_in serv_adr, clnt_adr;
-
     socklen_t clnt_adr_sz;
 
     if (argc != 2)
@@ -34,84 +31,79 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    serv_sock = socket(PF_INET, SOCK_STREAM, 0); // 调用 socket函数创建套接字。
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (serv_sock == -1)
     {
-        error_handing("socket() error");
+        error_handling("soket() error");
     }
 
-    // 初始化结构体
     memset(&serv_adr, 0, sizeof(serv_adr));
-    serv_adr.sin_family - AF_INET;
-    serv_adr.sin_addr.s_addr = htons(INADDR_ANY);
-    serv_adr.sin_port = (atoi(argv[1]));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serv_sock, (sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) // 调用 bind 函数分配IP地址和端口号。
+    if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1)
     {
-        error_handing("bind() error");
+        error_handling("bind() error");
     }
 
-    if (listen(serv_sock, 5) == -1) // 调用 listen 函数将套接字转为可接收连接状态。
+    if (listen(serv_sock, 5) == -1)
     {
-        error_handing("listen() error");
+        error_handling("listen() error");
     }
 
     clnt_adr_sz = sizeof(clnt_adr);
+
     for (i = 0; i < 5; i++)
     {
         opnd_cnt = 0;
-        clnt_sock = accept(serv_sock, (sockaddr *)&clnt_adr, &clnt_adr_sz);
-        read(clnt_sock, &opnd_cnt, 1); // 首先接收待算数个数 
+        clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+        read(clnt_sock, &opnd_cnt, 1); // 接收待算数个数
 
         recv_len = 0;
-        while ((opnd_cnt * OPSZ + 1) > recv_len)
+        while ((opnd_cnt * OPSZ + 1) > recv_len) // 根据待算数个数接收待算数
         {
-            recv_len = read(clnt_sock, &opinfo[recv_len], opinfo[recv_len - 1]);
+            recv_cnt = read(clnt_sock, &opinfo[recv_len], BUF_SIZE - 1);
             recv_len += recv_cnt;
         }
-
         result = calculate(opnd_cnt, (int *)opinfo, opinfo[recv_len - 1]);
-
-        write(clnt_sock, (char *)&result, sizeof(result));
+        write(clnt_sock, (char *)&result, sizeof(result)); // 向客户端传输运算结果
         close(clnt_sock);
     }
     close(serv_sock);
-
     return 0;
 }
 
-void error_handing(char *message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
-}
-
-int calculate(int opnum, int opnds[], char oprator)
+int calculate(int opnum, int opnds[], char op)
 {
     int result = opnds[0], i;
-    switch (oprator)
+    switch (op)
     {
     case '+':
-        for (i = 0; i < opnum; i++)
+        for (i = 1; i < opnum; i++)
         {
             result += opnds[i];
         }
-
         break;
     case '-':
-        for (i = 0; i < opnum; i++)
+        for (i = 1; i < opnum; i++)
         {
             result -= opnds[i];
         }
         break;
     case '*':
-        for (i = 0; i < opnum; i++)
+        for (i = 1; i < opnum; i++)
         {
             result *= opnds[i];
         }
         break;
     }
-
     return result;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
